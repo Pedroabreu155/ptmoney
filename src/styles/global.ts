4b6fe4a8457e49c9cbcf9c7dead4512be18eb6e8
Pedro Abreu <pedroliveirabreu@gmail.com>
2021-07-13 17:45:31 -0300

    added new font by google

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -37,6 +37,15 @@ export const GlobalStyles = createGlobalStyle`
     -webkit-font-font-smoothing: antialiased;
   }
 
+  border-style, input, textarea, button{
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong{
+    font-weight: 600
+  }
+
   button{
     cursor: pointer;
   }
