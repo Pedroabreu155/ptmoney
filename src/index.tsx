1a13964b6af7bee04444067ed111ce7d62c1e6d5
Pedro Abreu <pedroliveirabreu@gmail.com>
2021-07-17 16:58:09 -0300

    created new route post on fake api

--- src/index.tsx
+++ src/index.tsx
@@ -2,29 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
 
-import { createServer } from 'miragejs';
+import { createServer, Model } from 'miragejs';
 
 createServer({
+  models: {
+    transaction: Model,
+  },
+
   routes() {
     this.namespace = 'api';
 
     this.get('/transactions', () => {
-      return [
-        {
-          id: 1,
-          title: 'Transação 1',
-          amount: 420,
-          type: 'deposit',
-          category: 'Car',
-          createdAt: new Date(),
-        },
-      ];
+      return this.schema.all('transaction');
     });
 
     this.post('/transactions', (schema, request) => {
       const data = JSON.parse(request.requestBody);
 
-      return data;
+      return schema.create('transaction', data);
     });
   },
 });
